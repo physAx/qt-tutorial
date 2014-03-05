13a1b2b510cb07b9143c8e2daa8b1d9e3c9cecdc
physalis <chriskriebel@gmx.net>
2014-03-05 23:39:35 +0100

    First 2 Tutorials

---
+++ tutorial-qt2.py
@@ -0,0 +1,38 @@
+__author__ = 'physalis'
+
+import sys
+from PySide.QtCore import *
+from PySide.QtGui import *
+from math import *
+
+class Form(QDialog):
+
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+
+        self.browser = QTextBrowser()
+        self.lineedit = QLineEdit("Type an expression and press Enter")
+        self.lineedit.selectAll()
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.browser)
+        layout.addWidget(self.lineedit)
+        self.setLayout(layout)
+
+        self.lineedit.setFocus()
+
+        self.connect(self.lineedit, SIGNAL("returnPressed()"), self.updateUI)
+        self.setWindowTitle("Calculate")
+
+    def updateUI(self):
+        try:
+            text = self.lineedit.text()
+            self.browser.append("%s = <b>%s</b>" % (text, eval(text)))
+        except:
+            self.browser.append("<font color=red>%s is invalid</font>" % text)
+        self.lineedit.selectAll()
+
+app = QApplication(sys.argv)
+form = Form()
+form.show()
+app.exec_()
